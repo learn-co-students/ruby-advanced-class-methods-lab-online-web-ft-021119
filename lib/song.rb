7daef08fc5948e305bef19588b307cbe9fb54562
Vinh Do <vinhdo07@gmail.com>
2019-02-21 23:45:12 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -31,10 +31,40 @@ class Song
   end
 
   def self.find_by_name(name)
-    # binding.pry
     @@all.each do |index|
       return index if index.name == name
     end
-    false
+    nil
+  end #end of .find_by_name Class Method
+
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    if song == nil || song == false
+      song = self.create_by_name(name)
+    else
+      song
+    end
+  end #end of find_or_create
+
+  def self.alphabetical
+    @@all.sort_by! {|song| song.name}
   end
-end
+
+  def self.new_from_filename(filename)
+    file_ary = filename.split(/[-.]/)
+    # binding.pry
+    song = self.create_by_name(file_ary[1].strip!)
+    song.artist_name = file_ary[0].strip!
+    song
+  end #end of .new_from file
+
+  def self.create_from_filename(filename)
+    song = self.new_from_filename(filename)
+    song.save
+    song
+  end #end of .create_from_filename
+
+  def self.destroy_all
+    @@all.clear
+  end
+end #end of Class
