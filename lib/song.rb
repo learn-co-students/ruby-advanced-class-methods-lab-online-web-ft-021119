61fa64174afbccb239371e2fbd4905222d56dbbf
Taylor Swinton <taylorswinton1@gmail.com>
2019-02-22 05:15:12 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -33,8 +33,42 @@ class Song
     @@all.find{|song| song.name == findSong}
   end
 
-  def self.find_or_create_by_name(findSongTwo)
+  def self.find_or_create_by_name(song)
 
+      find_by_name(song) || create_by_name(song)
+
+=begin
+    findit = findSongTwo
+    if findit == find_by_name(findSongTwo)
+      find_by_name(findSongTwo)
+    else  findit != findSongTwo
+      create_by_name(findSongTwo)
+    end
+=end
+  end
+
+  def self.alphabetical
+    self.all.sort_by{|songs| songs.name}
+
+  end
+
+  def self.new_from_filename(file)
+    filenames = self.new
+    filenames.name = file.split(/[^a-zA-Z\s]|\s-\s/)[1]
+    filenames.artist_name = file.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    filenames
+  end
+
+  def self.create_from_filename(file)
+    filenametwo = self.new
+    filenametwo.name = file.split(/[^a-zA-Z\s]|\s-\s/)[1]
+    filenametwo.artist_name = file.split(/[^a-zA-Z\s]|\s-\s/)[0]
+    filenametwo.save
+    filenametwo
+  end
+
+  def self.destroy_all
+    self.all.clear
   end
 
 end
